987c81ac9d157b86e065f418950c0a5fcfdbe1fa
JMS <199091+jmsevere@users.noreply.github.com>
2025-10-20 15:05:09 -0700

    first build

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+//import { useEffect, useRef, useState } from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <main>Tetris</main>
+    </div>
+  );
+}
+
+export default App;
